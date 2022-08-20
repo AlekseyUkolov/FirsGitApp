0dd83c8193cf01a2bedeff7c22a2cc8f1e3c5261
AlekseyUkolov <akvadecor@gmail.com>
2022-08-20 17:40:25 +0400

    Creating project

--- app/src/main/java/com/example/firsgitapp/MainActivity.kt
+++ app/src/main/java/com/example/firsgitapp/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var str: String="emty"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // комментарий
     }
 }
